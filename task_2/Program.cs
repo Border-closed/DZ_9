e4d48ce7a9bb4f8eab5d9c485c45a633125a8ad6
Dmitrii <directsdi@gmail.com>
2022-11-20 23:14:50 +0500

    Задача 66. Сумма натуральных чисел от  N до M

---
+++ task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+//  Ввод числа N и M пользователем
+Console.WriteLine("Введите натуральное число M");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N");
+int N = int.Parse(Console.ReadLine());
+
+int sum = 0;
+for (int x = M; x < N+1; x++)
+    {
+     sum = sum+x;   
+    }
+Console.WriteLine("Сумма числел от " +M + " до " +N+" = "+sum);
